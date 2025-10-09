846d498931eb07cfabfafcb07870ec2e843fac04
Arturo <art2dit2@hotmail.com>
2025-10-09 16:31:28 +0200

    Conf useReduce and doing the func deleteteTodo, addTodo

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,10 @@ import { TrafficLightwithEffect } from './02-useEffect/TrafficLightWithEffect'
 import { TrafficLightwithHook } from './02-useEffect/TrafficLightWithHook'
 import { PokemonPage } from './03-Examples/PokemoPage'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FocusScreen />
+    <TasksApp />
   </StrictMode>,
 )
