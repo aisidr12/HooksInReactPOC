8555ae528415084e89ddc836d016658db25c4fc2
Arturo <art2dit2@hotmail.com>
2025-10-15 21:30:25 +0200

    Trying to do the scrambleWord

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,10 @@ import { TrafficLightwithHook } from './02-useEffect/TrafficLightWithHook'
 import { PokemonPage } from './03-Examples/PokemoPage'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { TasksApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TasksApp />
+    <ScrambleWords />
   </StrictMode>,
 )
