a7c82835800dc069777c7682f8dcb7ad41c2e1b1
Arturo <art2dit2@hotmail.com>
2026-01-06 18:56:20 +0100

    memo funciones

--- src/06-memos/MemoHook.tsx
+++ src/06-memos/MemoHook.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { MyTitle } from './ui/MyTitle'
 import { MySubTitle } from './ui/MySubTitle';
 
@@ -7,24 +7,31 @@ export const MemoHook = () => {
     const [title, setTitle] = useState("Hola");
     const [subTitle, setSubTitle] = useState("Mundo");
 
+    //Use Callback es como un useEffect que necesita de argumentos tambien 
+    // Solo se actualiza si en caso hay algún cambio
+    // no obstante memo y useCallback se prevee que haya cambios
+    const handleMyAPICall = useCallback(() => {
+      console.log("LLamar a mi API", subTitle);
+    }, [subTitle] );
+
   return (
    <div className='bg-gradient flex flex-col gap-4'>
-        <h1 className='text-2xl font-thin text-white'> MemoApp</h1>
+        <h1 className='text-2xl font-thin text-white'>MemoApp</h1>
  
   <MyTitle title={title} /> 
-  <MySubTitle subtitle={subTitle} />
 
+  <MySubTitle subtitle={subTitle}  callMyAPI={handleMyAPICall} />
  
  <h5> Mi Subtitulo </h5>
- 
-  <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'>
+   <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'
+    onClick={()=> setTitle("Hello, " + new Date().getTime())}
+   >
     Cambiar Titulo
   </button>
-   <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'>
+   <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'
+   onClick={ () => setSubTitle("World")}>
     Cambiar SubTitulo
   </button>
-  
-  
-  </div>
+    </div>
   )
 }
--- src/06-memos/ui/MySubTitle.tsx
+++ src/06-memos/ui/MySubTitle.tsx
@@ -3,19 +3,22 @@ import React from 'react'
 
 interface Props{
     subtitle: string;
+    callMyAPI: ()=> void;
 }
 
 
-export const MySubTitle = ({subtitle}:Props ) => {
+export const MySubTitle = React.memo(({subtitle, callMyAPI}:Props ) => {
     console.log('My sub title Re-render');
 
     return (
     <>
-    <h6> {subtitle}</h6>
-    <Button className='bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer'>
-    Llamar a funcion 
+      <h6 className="text-2xl font-bold"> {subtitle}</h6>
+    <Button className='bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer'
+      onClick={callMyAPI}
+          >
+      Llamar a funcion 
     </Button>
     </>
   
   )
-}
+});
